becf3f68438f8b3f44ef45b8dd2ab6e1139d9339
yashksaini <yashdms1@gmail.com>
2024-03-15 19:27:45 +0530

    feat: :tada: Project completed with loader on main screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Calendar from "./Calendar";
 import EmojiSelect from "./EmojiSelect";
@@ -9,31 +9,57 @@ function App() {
   const [savedemojis, setSavedemojis] = useState(
     JSON.parse(localStorage.getItem("savedemojis")) || []
   );
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
   return (
-    <div className="container">
-      <Calendar
-        selDate={selDate}
-        setSelDate={setSelDate}
-        savedemojis={savedemojis}
-      />
-      <EmojiSelect
-        selDate={selDate}
-        setSavedemojis={setSavedemojis}
-        savedemojis={savedemojis}
-      />
-      <div className="top-bar">
-        <img src="./emoji-calendar.png" />
-        <p>Emoji Calendar</p>
-      </div>
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        limit={3}
-        className="toastContainer"
-        closeButton={false}
-        hideProgressBar
-      />
-    </div>
+    <>
+      {!isLoading && (
+        <div className="container">
+          <Calendar
+            selDate={selDate}
+            setSelDate={setSelDate}
+            savedemojis={savedemojis}
+          />
+          <EmojiSelect
+            selDate={selDate}
+            setSavedemojis={setSavedemojis}
+            savedemojis={savedemojis}
+          />
+          <div className="top-bar">
+            <img src="./emoji-calendar.png" />
+            <p>Emoji Calendar</p>
+          </div>
+          <ToastContainer
+            position="top-center"
+            autoClose={2000}
+            limit={3}
+            className="toastContainer"
+            closeButton={false}
+            hideProgressBar
+          />
+        </div>
+      )}
+      {isLoading && (
+        <div className="loadingScreen">
+          <div className="loading-content">
+            <img src="./emoji-calendar.png" />
+            <h1>Emoji Calendar</h1>
+            <span className="loader"></span>
+            <p>© 2024 Copyright </p>
+            <p>
+              <a href="https://yashksaini.netlify.app/" target="_blank">
+                Yash Kumar Saini
+              </a>{" "}
+              All rights reserved
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
